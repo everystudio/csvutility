faa5db9c2889210dd313f6353e0d110dc45a231a
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-11-12 13:23:34 +0900

    Playerのデータを追加しました

--- Assets/Scenes/TestCsv.cs
+++ Assets/Scenes/TestCsv.cs
@@ -6,15 +6,32 @@ using csvutility;
 public class TestCsv : MonoBehaviour
 {
     public TextAsset m_textMasterEnemy;
+    public TextAsset m_textMasterPlayer;
 
     void Start()
     {
+        MasterPlayer player = new MasterPlayer();
+        player.Load(m_textMasterPlayer);
+        foreach( MasterPlayerParam p in player.list)
+        {
+            Debug.Log(string.Format("{0}:{1} HP={2}",
+                p.player_id,
+                p.player_name,
+                p.hp));
+        }
+
+
+
+        /*
         MasterEnemy enemy = new MasterEnemy();
         enemy.Load(m_textMasterEnemy);
-
         foreach(MasterEnemyParam p in enemy.list)
         {
             Debug.Log(string.Format("{0}:{1}", p.enemy_id, p.name));
         }
+        */
+
+
+
     }
 }
---
+++ Assets/data/MasterPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using csvutility;
+
+public class MasterPlayerParam : CsvDataParam
+{
+	public int player_id { get; set; }
+	public string player_name { get; set; }
+	public int hp { get; set; }
+}
+
+public class MasterPlayer : CsvData<MasterPlayerParam>
+{
+}
